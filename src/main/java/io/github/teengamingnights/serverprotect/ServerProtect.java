b90e11b4c5a506b408ad0060d5001a64bce5a41a
Devan-Kerman <devan@cleverpath.com>
2020-06-25 14:53:16 -0500

    yes

---
+++ src/main/java/io/github/teengamingnights/serverprotect/ServerProtect.java
@@ -0,0 +1,13 @@
+package io.github.teengamingnights.serverprotect;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class ServerProtect implements DedicatedServerModInitializer {
+	@Override
+	public void onInitializeServer() {
+		ServerWorld world;
+	}
+}
---
+++ src/main/java/io/github/teengamingnights/serverprotect/data/ThreadedHaybaleChunkDiffStorage.java
@@ -0,0 +1,7 @@
+package io.github.teengamingnights.serverprotect.data;
+
+import net.minecraft.world.World;
+
+public final class ThreadedHaybaleChunkDiffStorage {
+
+}
